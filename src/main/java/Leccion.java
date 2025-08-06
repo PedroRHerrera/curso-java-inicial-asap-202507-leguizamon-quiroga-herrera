5d14ffc231623a46ffab0dbc24a70ff2820734be
Gonzalo Quiroga <gonzaloquirogasassu@gmail.com>
2025-08-06 17:14:09 -0300

    add Leccion

---
+++ src/main/java/Leccion.java
@@ -0,0 +1,37 @@
+public class Leccion {
+    private String nombre;
+    private Integer duracion;
+    private TipoLeccion tipoLeccion;
+
+    public Leccion() {
+    }
+
+    public Leccion(String nombre, Integer duracion) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    public TipoLeccion getTipoLeccion() {
+        return tipoLeccion;
+    }
+
+    public void setTipoLeccion(TipoLeccion tipoLeccion) {
+        this.tipoLeccion = tipoLeccion;
+    }
+}
