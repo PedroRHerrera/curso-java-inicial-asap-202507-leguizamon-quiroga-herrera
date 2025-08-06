8db18bb8a3c92c11a89b2f726fa1df76f1b0c87e
PedroRHerrera <pedroherreravdp@gmail.com>
2025-08-06 17:17:49 -0300

    Creación de Categoria - Usuario - RespuestaSuscripcion

---
+++ src/main/java/com/github/bgp/java_inicial/clase05/_01_ejercicio/_ASAPDemy/Categoria.java
@@ -0,0 +1,13 @@
+package com.github.bgp.java_inicial.clase05._01_ejercicio._ASAPDemy;
+
+public class Categoria {
+
+	private String id;
+	private String nombre;
+	
+	
+	public Categoria(String id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+}
---
+++ src/main/java/com/github/bgp/java_inicial/clase05/_01_ejercicio/_ASAPDemy/Usuario.java
@@ -0,0 +1,19 @@
+package com.github.bgp.java_inicial.clase05._01_ejercicio._ASAPDemy;
+
+public class Usuario {
+
+	private String id;
+	private String nombre;
+	private String mail;
+	private boolean esBecado;
+	
+	public Usuario(String id, String nombre, String mail, boolean esBecado) {
+		this.id = id;
+		this.nombre = nombre;
+		this.mail = mail;
+		this.esBecado = esBecado;
+	}
+	
+	
+	
+}
